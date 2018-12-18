0f80dc760be07c8053615ae7e633082fdb580497
Bruce W. Clounie <bruce.clounie@nextworld.net>
2018-12-17 21:08:39 -0700

    Let's piss off sonarlint

--- src/main/java/net/a/test/MyTestClass.java
+++ src/main/java/net/a/test/MyTestClass.java
@@ -2,11 +2,19 @@ package net.a.test;
 
 import net.a.separatepackage.AnUnusedImport;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyTestClass {
   public static void main(String[] args) {
     System.out.println("Testing things");
 
 //    AnUnusedImport.doThings();
+    List<String> myList = new ArrayList<>();
+            String b = "hi";
+    for (String a: myList) {
+      b += a;
+    }
   }
 
   public static String getAString () {
